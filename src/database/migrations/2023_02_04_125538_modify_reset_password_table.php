92081d5dcb2995ce64f1717efac3130b6d15dd5b
Mehran Ranji <mehran.ranji@gmail.com>
2025-12-01 15:00:10 +0330

    has passwords table

--- src/database/migrations/2023_02_04_125538_modify_reset_password_table.php
+++ src/database/migrations/2023_02_04_125538_modify_reset_password_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table(config("auth.passwords.users.table"), function (Blueprint $table) {
+        if (Schema::hasTable(config("auth.passwords.users.table"))) Schema::table(config("auth.passwords.users.table"), function (Blueprint $table) {
             $table->nullableMorphs('user');
         });
     }
